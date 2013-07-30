5bc86a4967e5ebf5986fc06b109bbe24d3d5d371
socialchorus <developers@socialchorus.com>
2013-07-30 12:24:36 -0700

    removing the rails check on railtie

--- lib/gta/railtie.rb
+++ lib/gta/railtie.rb
@@ -1,9 +1,7 @@
-if defined?(Rails)
-  module GTA
-    class Railtie < Rails::Railtie
-      rake_tasks do
-        require File.dirname(__FILE__) + "/tasks.rb"
-      end
+module GTA
+  class Railtie < Rails::Railtie
+    rake_tasks do
+      require File.dirname(__FILE__) + "/tasks.rb"
     end
   end
 end
